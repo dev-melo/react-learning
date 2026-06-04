350dfac3ee148fc76c37613aab0eceb9b09807b4
Dev Melo <omelodev@gmail.com>
2026-06-04 14:50:51 -0300

    feat: o que é um componente react, um hook, uma função que retorna html react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+
+const Tests = () => { // Component Funcional React - a Primeira letra de um Componente precisa ser Maiuscula!!!
+  return (
+    <div>
+      Isso é um componente react criado por mim.
+    </div>
+  )
+}
+const tests = () => { // Função JS que retorna html react
+  return (
+    <div>
+      Isso é uma Função que retona html react.
+    </div>
+  )
+}
+
 export function App() {
 
   return (
-    <>
+    <div>
     Olá
-    </>
+    <Tests></Tests>
+    <p>---</p>
+    {tests()} 
+    </div>
   )
 }
+
+
+const teste = () => { // Função JS
+  return 1 + 1;
+}
+
+const useTeste = () => { // Função JS
+  return 1 + 1;
+}
+const useTest = () => { // React hook 
+  const [value] =  useState( 1 + 1 )
+  return value;
+}
+
