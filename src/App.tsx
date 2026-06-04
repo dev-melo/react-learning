4bee6742856a91b38df6ef628b5f5e9e634da127
Dev Melo <omelodev@gmail.com>
2026-06-04 17:40:49 -0300

    feat: inserindo novos itens na lista de forma dinamica

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 export function App() { // componente base, (APP) porta de entrada da aplicação
 
+  const [value, setValue] = useState('');
   const [list, setList] = useState([
     {id: '1', label: 'Fazer café',},
     {id: '2', label: 'Fazer café',},
@@ -12,11 +13,20 @@ export function App() { // componente base, (APP) porta de entrada da aplicaçã
   return (
     <div>
 
-      <input type="text" />
-      <button>Add</button>
+      <input 
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={() => {
+        setList([...list, { id: (list.length + 1).toString(), label: value }]);
+        setValue('');
+      }}>
+          Add
+        </button>
       <ol>
         {list.map((listItem) => (
-          <li key={listItem.id}>{listItem.label}</li>
+          <li key={listItem.id}>
+            {listItem.label}</li>
         ))}
       </ol>
     </div>
